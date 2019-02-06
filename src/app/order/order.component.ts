55d3e270d43257023c3325949a9ff2f5a7ab610c
Yves Guilherme Lopo Ribeiro Lima <yveslopo@gmail.com>
2019-02-06 21:43:58 -0200

    Implementação do componente order-summary (Conclusão do pedido)
    Signed-off-by: Yves Guilherme Lopo Ribeiro Lima <yveslopo@gmail.com>

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { RadioOption } from 'app/shared/radio/radio-option.model';
 import { OrderService } from './order.service';
 import { CartItem } from 'app/restaurant-detail/shopping-cart/cart-item.model';
+import { Order, OrderItem } from './order.model';
 
 @Component({
   selector: 'mt-order',
@@ -19,7 +21,7 @@ export class OrderComponent implements OnInit {
     { label: 'Cartão de Refeição', value: 'REF' }
   ];
 
-  constructor(private orderService: OrderService) { }
+  constructor(private orderService: OrderService, private router: Router) { }
 
   ngOnInit() { }
 
@@ -41,6 +43,18 @@ export class OrderComponent implements OnInit {
 
   remove(item: CartItem) {
     this.orderService.remove(item);
+  }
+
+  checkOrder(order: Order) {
+    order.orderItems = this.cartItems()
+      .map((item: CartItem) => new OrderItem(item.quantity, item.menuItem.id));
+    this.orderService
+      .checkOrder(order)
+      .subscribe((orderId: string) => {
+        this.router.navigate(['/order-summary']);
+        this.orderService.clear();
+      });
+    console.log(order);
   }
 
 }
--- src/app/order/order.service.ts
+++ src/app/order/order.service.ts
@@ -1,12 +1,19 @@
 import { Injectable } from '@angular/core';
 
+import { Http, Headers, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
 import { ShoppingCartService } from 'app/restaurant-detail/shopping-cart/shopping-cart.service';
 import { CartItem } from 'app/restaurant-detail/shopping-cart/cart-item.model';
+import { Order } from './order.model';
+
+import { MEAT_API } from 'app/app.api';
 
 @Injectable()
 export class OrderService {
 
-    constructor(private cartService: ShoppingCartService) { }
+    constructor(private cartService: ShoppingCartService, private http: Http) { }
 
     itemsValue(): number {
         return this.cartService.total();
@@ -26,5 +33,22 @@ export class OrderService {
 
     remove(item: CartItem) {
         this.cartService.removeItem(item);
+    }
+
+    /**
+     * Limpa os itens do carrinho
+     * após finalizar a compra.
+     */
+    clear() {
+        this.cartService.clear();
+    }
+
+    checkOrder(order: Order): Observable<string> {
+        const headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+        return this.http
+            .post(`${MEAT_API}/orders`, JSON.stringify(order), new RequestOptions({ headers: headers }))
+            .map(response => response.json())
+            .map(order => order.id);
     }
 }

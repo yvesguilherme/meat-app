2904c55952e7742c086ecd22271b112cc2c8e4c0
Yves Guilherme Lopo Ribeiro Lima <yveslopo@gmail.com>
2019-02-06 22:16:10 -0200

    Implementação do componente de rating (Avaliação de experiência do usuário)
    Signed-off-by: Yves Guilherme Lopo Ribeiro Lima <yveslopo@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,6 +26,7 @@ import { OrderItemsComponent } from './order/order-items/order-items.component';
 import { OrderService } from './order/order.service';
 import { DeliveryCostsComponent } from './order/delivery-costs/delivery-costs.component';
 import { OrderSummaryComponent } from './order-summary/order-summary.component';
+import { RatingComponent } from './shared/rating/rating.component';
 
 
 @NgModule({
@@ -47,6 +48,7 @@ import { OrderSummaryComponent } from './order-summary/order-summary.component';
     OrderItemsComponent,
     DeliveryCostsComponent,
     OrderSummaryComponent,
+    RatingComponent,
   ],
   imports: [
     BrowserModule,

5fd5e9989e416f48e809182bd4e69b8e2a367e87
Yves Guilherme Lopo Ribeiro Lima <yveslopo@gmail.com>
2019-02-09 09:39:52 -0200

    Adicionando animação com Void e estilos em transições no componente Restaurant

--- src/app/shared/messages/snackbar/snackbar.component.ts
+++ src/app/shared/messages/snackbar/snackbar.component.ts
@@ -12,32 +12,18 @@ import 'rxjs/add/operator/switchMap';
   selector: 'mt-snackbar',
   templateUrl: './snackbar.component.html',
   styleUrls: ['./snackbar.component.css'],
-  animations: [
-    trigger('snack-visibility', [
-      state(
-        'hidden',
-        style({
-          opacity: 0,
-          bottom: '0'
-        })
-      ),
-      state(
-        'visible',
-        style({
-          opacity: 1,
-          bottom: '30px'
-        })
-      ),
-      transition(
-        'hidden => visible',
-        animate('500ms 0s ease-in')
-      ),
-      transition(
-        'visible => hidden',
-        animate('500ms 0s ease-out')
+  animations:
+    [
+      trigger(
+        'snack-visibility',
+        [
+          state('hidden', style({ opacity: 0, bottom: '0' })),
+          state('visible', style({ opacity: 1, bottom: '30px' })),
+          transition('hidden => visible', animate('500ms 0s ease-in')),
+          transition('visible => hidden', animate('500ms 0s ease-out'))
+        ]
       )
-    ])
-  ]
+    ]
 })
 export class SnackbarComponent implements OnInit {
 

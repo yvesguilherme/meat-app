98fafc7ad397437c79135fc06e2b046b1d387be7
Yves Guilherme Lopo Ribeiro Lima <yveslopo@gmail.com>
2019-02-09 21:43:12 -0200

    Adicionando animação no componente menu-item

--- src/app/restaurant-detail/menu-item/menu-item.component.ts
+++ src/app/restaurant-detail/menu-item/menu-item.component.ts
@@ -1,12 +1,31 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 import { MenuItem } from './menu-item.model';
 
 @Component({
   selector: 'mt-menu-item',
-  templateUrl: './menu-item.component.html'
+  templateUrl: './menu-item.component.html',
+  animations:
+    [
+      trigger(
+        'menuItemAppeared',
+        [
+          state('ready', style({ opacity: 1 })),
+          transition(
+            'void => ready',
+            [
+              style({ opacity: 0, transform: 'translateY(-20px' }),
+              animate('400ms 0s ease-in')
+            ]
+          )
+        ]
+      )
+    ]
 })
 export class MenuItemComponent implements OnInit {
+
+  menuItemState = 'ready';
 
   @Input()
   menuItem: MenuItem;

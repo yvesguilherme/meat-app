c118e0c172a896601c36a6a0b44fc5f97f20a823
Yves Guilherme Lopo Ribeiro Lima <yveslopo@gmail.com>
2019-02-12 23:17:24 -0200

    Middleware de Autorização.

--- backend/auth.ts
+++ backend/auth.ts
@@ -2,13 +2,14 @@ import { Request, Response } from 'express';
 import { User, users } from './users';
 
 import * as jwt from 'jsonwebtoken';
+import { apiConfig } from './api-config';
 
 /** Trata as respostas. */
 export const handleAuthentication = (req: Request, resp: Response) => {
   const user: User = req.body;
   if (isValid(user)) {
     const dbUser = users[user.email];
-    const token = jwt.sign({ sub: dbUser.email, iss: 'meat-api' }, 'meat-api-password');
+    const token = jwt.sign({ sub: dbUser.email, iss: 'meat-api' }, apiConfig.secret);
     resp.json({ name: dbUser.name, email: dbUser.email, accessToken: token });
   } else {
     resp.status(403).json({ message: 'Dados inválidos.' });
--- backend/server.ts
+++ backend/server.ts
@@ -5,6 +5,7 @@ import * as fs from 'fs';
 import * as https from 'https';
 
 import { handleAuthentication } from './auth';
+import { handleAuthorization } from './authz';
 
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
@@ -19,6 +20,7 @@ server.use(jsonServer.bodyParser);
 
 // Middleware para login
 server.post('/login', handleAuthentication);
+server.use('/orders', handleAuthorization);
 
 // Use default router
 server.use(router);

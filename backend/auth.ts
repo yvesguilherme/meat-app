7f08c81b15307a531c22f3ed968abac767e59643
Yves Guilherme Lopo Ribeiro Lima <yveslopo@gmail.com>
2019-02-12 22:20:37 -0200

    Implementando a Rota de Login

---
+++ backend/auth.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from 'express';
+
+import { User, users } from './users';
+
+/** Trata as respostas. */
+export const handleAuthentication = (req: Request, resp: Response) => {
+  const user: User = req.body;
+  if (isValid(user)) {
+    const dbUser: User = users[user.email];
+    resp.json({ name: dbUser.name, email: dbUser.email });
+  } else {
+    resp.status(403).json({ message: 'Dados inválidos.' });
+  }
+}
+/** Verifica se o usuário é válido. */
+function isValid(user: User): boolean {
+  if (!user) {
+    return false;
+  }
+  const dbUser = users[user.email];
+  return dbUser !== undefined && dbUser.matches(user);
+}

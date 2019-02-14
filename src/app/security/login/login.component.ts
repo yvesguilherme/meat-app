e7d02e24893d79c95d812e43783ef89420c8fd06
Yves Guilherme Lopo Ribeiro Lima <yveslopo@gmail.com>
2019-02-14 16:41:37 -0200

    Adicionando autenticação na compra e mensagens de erro ou de sucesso.

--- src/app/security/login/login.component.ts
+++ src/app/security/login/login.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { LoginService } from './login.service';
 import { User } from './user.model';
+import { NotificationService } from 'app/shared/messages/notification.service';
 
 @Component({
   selector: 'mt-login',
@@ -13,7 +14,12 @@ export class LoginComponent implements OnInit {
 
   loginForm: FormGroup;
 
-  constructor(private fb: FormBuilder, private loginService: LoginService) { }
+  constructor
+    (
+      private fb: FormBuilder,
+      private loginService: LoginService,
+      private notificationService: NotificationService
+    ) { }
 
   ngOnInit() {
     this.loginForm = this.fb.group({
@@ -25,7 +31,10 @@ export class LoginComponent implements OnInit {
   login() {
     this.loginService
       .login(this.loginForm.value.email, this.loginForm.value.password)
-      .subscribe(user => console.log(user));
+      .subscribe(user => this.notificationService
+        .notify(`Bem-vindo (a), ${user.name}.`),
+        response => // Este response é do tipo HttpErrorResponse
+          this.notificationService.notify(response.error.message));
   }
 
 }

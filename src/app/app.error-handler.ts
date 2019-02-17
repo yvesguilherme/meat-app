7ff34cc3739e0156fce91d6c3ea1359440177c71
Yves Guilherme Lopo Ribeiro Lima <yveslopo@gmail.com>
2019-02-17 02:30:07 -0300

    Adicionando o serviço de zona para resolver o problema das notificações de erro que estavam fora da zona do Angular

--- src/app/app.error-handler.ts
+++ src/app/app.error-handler.ts
@@ -1,4 +1,4 @@
-import { ErrorHandler, Injectable, Injector } from '@angular/core';
+import { ErrorHandler, Injectable, Injector, NgZone } from '@angular/core';
 import { HttpErrorResponse } from '@angular/common/http';
 import 'rxjs/add/observable/throw';
 
@@ -8,24 +8,30 @@ import { LoginService } from './security/login/login.service';
 @Injectable()
 export class ApplicationErrorHandler extends ErrorHandler {
 
-  constructor(private notificationService: NotificationService, private injector: Injector) {
+  constructor(
+    private notificationService: NotificationService,
+    private injector: Injector,
+    private zone: NgZone
+  ) {
     super();
   }
 
   handleError(errorResponse: HttpErrorResponse | any) {
     if (errorResponse instanceof HttpErrorResponse) {
       const message = errorResponse.error.message;
-      switch (errorResponse.status) {
-        case 401:
-          this.injector.get(LoginService).handleLogin();
-          break;
-        case 403:
-          this.notificationService.notify(message || 'Não autorizado.');
-          break;
-        case 404:
-          this.notificationService.notify(message || 'Recurso não encontrado. Verifique o console para mais detalhes.');
-          break;
-      }
+      this.zone.run(() => {
+        switch (errorResponse.status) {
+          case 401:
+            this.injector.get(LoginService).handleLogin();
+            break;
+          case 403:
+            this.notificationService.notify(message || 'Não autorizado.');
+            break;
+          case 404:
+            this.notificationService.notify(message || 'Recurso não encontrado. Verifique o console para mais detalhes.');
+            break;
+        }
+      })
     }
     super.handleError(errorResponse);
   }
